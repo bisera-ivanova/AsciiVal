cd6862cb0f8b2a2538ad2f70cff0eb29c3ada70e
Bibi <chichi11mythmaster@gmail.com>
2023-01-04 23:22:59 +0200

    Done - Page redirect - conditional display of results To Do - form resubmission from results page + front end

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-from flask import Flask, render_template, request
+import secrets
+
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex()
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        input_text = request.form["input_text"]
+        input_text = request.form["text_field"]
         bad_chars = check_ascii(input_text)
 
-        return render_template("index.html", input_text=input_text, bad_chars=bad_chars)
-    return render_template("index.html", input_text=None, bad_chars=None)
+        session['input_text'] = input_text
+        session['bad_chars'] = bad_chars
+
+        return redirect('/result')
+    return render_template("index.html")
 
 
 def check_ascii(testable_string):
-    bad_chars = []
+    bad_chars = {}
     for i, symbol in enumerate(testable_string):
         if not (ord(symbol) in range(0, 127)):
-            bad_chars.append((i, symbol))
-        print(ord(symbol))
+            bad_chars[i] = symbol
     return bad_chars
 
 
 @app.route("/result")
 def result():
-    pass
+    if len(session['bad_chars']) > 0:
+        return render_template('result.html', bad_chars=session['bad_chars'].items(), input_text=session['input_text'])
+    else:
+        return render_template('no_chars.html', input_text=session['input_text'])
 
 
 if __name__ == "__main__":
